d4bbc4d2189a1a4fc053b2a0b9ddbd158162cdbe
arasimohan <arasi.learn@gmail.com>
2025-03-11 12:08:08 +1030

    Added code for loading properties file and reading browser and test URL from config.properties file

---
+++ Citrus/OrangeHrm/src/test/java/arasi/test/FirstTest.java
@@ -0,0 +1,12 @@
+package arasi.test;
+
+import arasi.pages.TestBase;
+
+public class FirstTest {
+
+    public static void main(String[] args){
+        TestBase tst = new TestBase();
+
+        tst.loadProperties();
+    }
+}
